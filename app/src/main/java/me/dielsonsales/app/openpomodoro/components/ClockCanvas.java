c3ffa209501f823e8fbc0e38740c30f394fe4671
Dielson Sales <dielsonscarvalho@gmail.com>
2015-12-13 15:32:05 -0300

    Refactor ClockCanvas methods
    Joining some methods and moving some instructions to constructor

--- app/src/main/java/me/dielsonsales/app/openpomodoro/components/ClockCanvas.java
+++ app/src/main/java/me/dielsonsales/app/openpomodoro/components/ClockCanvas.java
@@ -20,11 +20,11 @@ import java.util.List;
 public class ClockCanvas extends View {
 
     // Abstract members
-    static final String TAG = "ClockCanvas";
-    static final double MINUTE_LENGTH_MOD = 0.8; // minutes pointer size
-    static final double HOUR_LENGTH_MOD = 0.5; // hours pointer size
-    static final int CURRENT_THICKNESS = 20; // current pomodoro mark thickness
-    static final int BORDER_THICKNESS = 15;
+    static final private String TAG = "ClockCanvas";
+    static final private double MINUTE_LENGTH_MOD = 0.8; // minutes pointer size
+    static final private double HOUR_LENGTH_MOD = 0.5; // hours pointer size
+    static final private int CURRENT_THICKNESS = 20; // current pomodoro mark thickness
+    static final private int BORDER_THICKNESS = 15;
     static public int CLOCK_COLOR;
     static public int POINTERS_COLOR;
     static public int INTERVAL_COLOR;
@@ -37,8 +37,8 @@ public class ClockCanvas extends View {
     // Elements of the clock
     private int mHours;
     private int mMinutes;
-    private List<Interval> mIntervals;
-    private Interval mCurrentPomodoro;
+    private List<TimeInterval> mIntervals;
+    private TimeInterval mCurrentPomodoro;
 
     // constructor ---------------------------------------------------------------------------------
 
@@ -50,7 +50,18 @@ public class ClockCanvas extends View {
         CLOCK_COLOR = Color.RED;
         POINTERS_COLOR = Color.WHITE;
         INTERVAL_COLOR = Color.GRAY;
-        POINTERS_COLOR = Color.YELLOW;
+        POMODORO_COLOR = Color.YELLOW;
+
+        // TODO: delete this later
+        List<TimeInterval> timeIntervals = new ArrayList<>();
+        Calendar startInterval = new GregorianCalendar();
+        startInterval.set(Calendar.HOUR, 0);
+        startInterval.set(Calendar.MINUTE, 0);
+        Calendar endInterval = new GregorianCalendar();
+        endInterval.set(Calendar.HOUR, 1);
+        endInterval.set(Calendar.MINUTE, 0);
+        timeIntervals.add(new TimeInterval(startInterval, endInterval));
+        this.addIntervals(timeIntervals);
     }
 
     // getters & setters ---------------------------------------------------------------------------
@@ -75,8 +86,12 @@ public class ClockCanvas extends View {
         return time;
     }
 
-    public void setClockColor() {
+    public void addIntervals(List<TimeInterval> intervals) {
+        mIntervals.addAll(intervals);
+    }
 
+    public void clearIntervals() {
+        mIntervals.clear();
     }
 
     // overriden methods ---------------------------------------------------------------------------
@@ -93,14 +108,8 @@ public class ClockCanvas extends View {
         mPaint.setColor(CLOCK_COLOR);
         canvas.drawCircle(mViewWidth / 2, mViewHeight / 2, mClockRadius, mPaint);
 
-        // Draws a time interval
-        Calendar startInterval = new GregorianCalendar();
-        startInterval.set(Calendar.HOUR, 0);
-        startInterval.set(Calendar.MINUTE, 0);
-        Calendar endInterval = new GregorianCalendar();
-        endInterval.set(Calendar.HOUR, 1);
-        endInterval.set(Calendar.MINUTE, 0);
-        drawInterval(startInterval, endInterval, canvas);
+        // Draws the timeIntervals
+        drawInterval(canvas);
 
         // Draws current pomodoro
         Calendar startTime = new GregorianCalendar();
@@ -116,7 +125,7 @@ public class ClockCanvas extends View {
         drawClockBorder(canvas);
     }
 
-    // private methods -----------------------------------------------------------------------------
+    // draw methods --------------------------------------------------------------------------------
 
     /**
      * Draws the clock hours and minutes pointers based on the hours and minutes values passed.
@@ -125,18 +134,7 @@ public class ClockCanvas extends View {
      * @param canvas the canvas where the pointers will be drawn
      */
     private void drawClockPointers(int hours, int minutes, Canvas canvas) {
-        drawHour(hours, minutes, canvas);
-        drawMinutes(minutes, canvas);
-        drawCentralCircle(canvas);
-    }
-
-    /**
-     * Draws the hour pinter in the clock
-     * @param hours the current hour
-     * @param minutes the current minutes
-     * @param canvas the canvas where the pointer will be drawn
-     */
-    private void drawHour(int hours, int minutes, Canvas canvas) {
+        // Draw hour
         double degreesAngle = calculateHourAngle(hours, minutes);
         double radiansAngle = degreesToRadians(degreesAngle);
         double hourBraceLength = mClockRadius * HOUR_LENGTH_MOD;
@@ -147,27 +145,20 @@ public class ClockCanvas extends View {
         mPaint.setStrokeWidth(5 * getResources().getDisplayMetrics().density);
         mPaint.setColor(POINTERS_COLOR);
         canvas.drawLine(mViewWidth / 2, mViewHeight / 2, x1, y1, mPaint);
-    }
 
-    /**
-     * Draws the minutes pointer in the clock
-     * @param minutes the current minutes
-     * @param canvas the canvas where the pointer will be drawn
-     */
-    private void drawMinutes(int minutes, Canvas canvas) {
-        double degreesAngle = calculateMinutesAngle(minutes);
-        double radiansAngle = degreesToRadians(degreesAngle);
+        // Draw minutes
+        degreesAngle = calculateMinutesAngle(minutes);
+        radiansAngle = degreesToRadians(degreesAngle);
         double minutesBraceLength = mClockRadius * MINUTE_LENGTH_MOD;
-        int x1 = (int) (mViewWidth /2 + minutesBraceLength * Math.sin(radiansAngle));
-        int y1 = (int) (mViewHeight /2 - minutesBraceLength * Math.cos(radiansAngle));
+        x1 = (int) (mViewWidth /2 + minutesBraceLength * Math.sin(radiansAngle));
+        y1 = (int) (mViewHeight /2 - minutesBraceLength * Math.cos(radiansAngle));
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setStrokeCap(Paint.Cap.ROUND);
         mPaint.setStrokeWidth(5 * getResources().getDisplayMetrics().density);
         mPaint.setColor(POINTERS_COLOR);
         canvas.drawLine(mViewWidth / 2, mViewHeight / 2, x1, y1, mPaint);
-    }
 
-    private void drawCentralCircle(Canvas canvas) {
+        // Draw central circle
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setColor(POINTERS_COLOR);
         canvas.drawCircle(mViewWidth / 2, mViewHeight / 2, 5 * getResources().getDisplayMetrics().density, mPaint);
@@ -175,22 +166,25 @@ public class ClockCanvas extends View {
 
     /**
      * Draws an interval of executed tasks
-     * @param startTime the time when the task began
-     * @param endTime the time when the task was finished
      * @param canvas the canvas where the interval will be drawn
      */
-    private void drawInterval(Calendar startTime, Calendar endTime, Canvas canvas) {
-        int x1 = mViewWidth /2 - mClockRadius /2;
-        int y1 = mViewHeight /2 - mClockRadius /2;
-        int x2 = mViewWidth /2 + mClockRadius /2;
-        int y2 = mViewHeight /2 + mClockRadius /2;
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeCap(Paint.Cap.BUTT);
-        mPaint.setStrokeWidth(15 * getResources().getDisplayMetrics().density);
-        mPaint.setColor(INTERVAL_COLOR);
-        int startAngle = (int) (calculateHourAngle(startTime.get(Calendar.HOUR), startTime.get(Calendar.MINUTE)));
-        int endAngle = (int) (calculateHourAngle(endTime.get(Calendar.HOUR), endTime.get(Calendar.MINUTE)));
-        canvas.drawArc(new RectF(x1, y1, x2, y2), startAngle - 90, endAngle - startAngle, false, mPaint);
+    private void drawInterval(Canvas canvas) {
+        for (TimeInterval timeInterval : mIntervals) {
+            Calendar startTime = timeInterval.getStartTime();
+            Calendar endTime = timeInterval.getEndTime();
+
+            int x1 = mViewWidth /2 - mClockRadius /2;
+            int y1 = mViewHeight /2 - mClockRadius /2;
+            int x2 = mViewWidth /2 + mClockRadius /2;
+            int y2 = mViewHeight /2 + mClockRadius /2;
+            mPaint.setStyle(Paint.Style.STROKE);
+            mPaint.setStrokeCap(Paint.Cap.BUTT);
+            mPaint.setStrokeWidth(15 * getResources().getDisplayMetrics().density);
+            mPaint.setColor(INTERVAL_COLOR);
+            int startAngle = (int) (calculateHourAngle(startTime.get(Calendar.HOUR), startTime.get(Calendar.MINUTE)));
+            int endAngle = (int) (calculateHourAngle(endTime.get(Calendar.HOUR), endTime.get(Calendar.MINUTE)));
+            canvas.drawArc(new RectF(x1, y1, x2, y2), startAngle - 90, endAngle - startAngle, false, mPaint);
+        }
     }
 
     /**
@@ -235,6 +229,8 @@ public class ClockCanvas extends View {
         canvas.drawArc(new RectF(x1, y1, x2, y2), 0, 360, false, mPaint);
     }
 
+    // utils methods -------------------------------------------------------------------------------
+
     /**
      * Calculates the hours pointer's angle, in degrees.
      * @param hour the current hour
@@ -252,21 +248,24 @@ public class ClockCanvas extends View {
      * @param minutes the current minutes
      * @return the angle of the minutes pointer to be drawn
      */
-    private double calculateMinutesAngle(int minutes) {
-        return 2.0 * 180.0 * minutes / 60.0;
-    }
-
-    private double degreesToRadians(double degrees) {
-        return (Math.PI/180) * degrees;
-    }
+    private double calculateMinutesAngle(int minutes) { return 2.0 * 180.0 * minutes / 60.0; }
 
     /**
-     * Represents a time interval that will be drawn as an arc in the clock.
+     * Converts a degrees angle value to its corresponding radian value.
+     * @param degrees the value in degrees
+     * @return the corresponding angle in radians
      */
-    public class Interval {
+    private double degreesToRadians(double degrees) { return (Math.PI/180) * degrees; }
+
+    // nested classes ------------------------------------------------------------------------------
+
+    /**
+     * Represents a time interval with a start and an end.
+     */
+    public class TimeInterval {
         private Calendar mStartTime;
         private Calendar mEndTime;
-        public Interval(Calendar startTime, Calendar endTime) {
+        public TimeInterval(Calendar startTime, Calendar endTime) {
             mStartTime = startTime;
             mEndTime = endTime;
         }

7ab6120572fe03febc79587209f52bcbb1428cae
Dielson Sales <dielsonscarvalho@gmail.com>
2015-12-16 21:32:29 -0300

    Add testTime to ClockCanvasTest

--- app/src/androidTest/java/me/dielsonsales/app/openpomodoro/components/ClockCanvasTest.java
+++ app/src/androidTest/java/me/dielsonsales/app/openpomodoro/components/ClockCanvasTest.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.SmallTest;
 
+import java.util.Calendar;
+
 import me.dielsonsales.app.openpomodoro.MainActivity;
 
 public class ClockCanvasTest extends ActivityInstrumentationTestCase2<MainActivity> {
@@ -33,6 +35,15 @@ public class ClockCanvasTest extends ActivityInstrumentationTestCase2<MainActivi
     @SmallTest
     public void testColors() {
         assertEquals(mClockCanvas.POMODORO_COLOR, Color.YELLOW);
+        assertEquals(mClockCanvas.POINTERS_COLOR, Color.WHITE);
     }
 
+    @SmallTest
+    public void testTime() {
+        Calendar currentTime = Calendar.getInstance();
+        mClockCanvas.setTime(currentTime);
+        Calendar expectedTime = mClockCanvas.getTime();
+        assertEquals(currentTime.get(Calendar.HOUR), expectedTime.get(Calendar.HOUR));
+        assertEquals(currentTime.get(Calendar.MINUTE), expectedTime.get(Calendar.MINUTE));
+    }
 }

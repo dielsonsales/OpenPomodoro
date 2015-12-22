75d2ac161bc3f7fbed49a93b5ad26c1f791c8d5b
Dielson Sales <dielsonscarvalho@gmail.com>
2015-12-22 03:21:16 -0300

    Running in foreground when pomodoro is active

--- app/src/main/java/me/dielsonsales/app/openpomodoro/controllers/PomodoroNotificationManager.java
+++ app/src/main/java/me/dielsonsales/app/openpomodoro/controllers/PomodoroNotificationManager.java
@@ -1,32 +1,35 @@
 package me.dielsonsales.app.openpomodoro.controllers;
 
 import android.app.NotificationManager;
+import android.app.Service;
 import android.content.Context;
 import android.support.v4.app.NotificationCompat;
 
 import me.dielsonsales.app.openpomodoro.R;
 
 /**
+ * The Notification manager. This class starts and stops the service from
+ * running in foreground, depending if the pomodoro is running or not.
  * Created by dielson on 21/12/15.
  */
 public class PomodoroNotificationManager {
     NotificationManager mNotificationManager;
-    Context mContext;
+    Service mService;
 
     public PomodoroNotificationManager(Context context) {
-        mContext = context;
-        mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        mService = (Service) context;
+        mNotificationManager = (NotificationManager) mService.getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     public void showNotification() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mService);
         builder.setContentTitle("OpenPomodoro");
         builder.setContentText("Pomodoro is running");
         builder.setSmallIcon(R.mipmap.ic_launcher);
-        mNotificationManager.notify(1, builder.build());
+        mService.startForeground(1, builder.build());
     }
 
     public void hideNotification() {
-        mNotificationManager.cancel(1);
+        mService.stopForeground(true);
     }
 }

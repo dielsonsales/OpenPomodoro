534d6b864af1c78324fe413ab46196d517b5bc61
Dielson Sales <dielsonscarvalho@gmail.com>
2015-12-27 17:00:05 -0300

    Open MainActivity when clicking the service notification

--- app/src/main/java/me/dielsonsales/app/openpomodoro/controllers/PomodoroNotificationManager.java
+++ app/src/main/java/me/dielsonsales/app/openpomodoro/controllers/PomodoroNotificationManager.java
@@ -1,10 +1,15 @@
 package me.dielsonsales.app.openpomodoro.controllers;
 
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 
+import me.dielsonsales.app.openpomodoro.MainActivity;
 import me.dielsonsales.app.openpomodoro.R;
 
 /**
@@ -23,13 +28,20 @@ public class PomodoroNotificationManager {
 
     public void showNotification() {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(mService);
+        builder.setCategory(Notification.CATEGORY_SERVICE);
         builder.setContentTitle("OpenPomodoro");
         builder.setContentText("Pomodoro is running");
         builder.setSmallIcon(R.mipmap.ic_launcher);
+
+        Intent resultIntent = new Intent(mService, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(mService);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(resultPendingIntent);
+
         mService.startForeground(1, builder.build());
     }
 
-    public void hideNotification() {
-        mService.stopForeground(true);
-    }
+    public void hideNotification() { mService.stopForeground(true); }
 }

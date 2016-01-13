f52c12d9261ebe5f71f501a2132f643deb55c522
Dielson Sales <dielsonscarvalho@gmail.com>
2016-01-13 01:04:01 -0300

    Add about screen

---
+++ app/src/main/java/me/dielsonsales/app/openpomodoro/AboutActivity.java
@@ -0,0 +1,23 @@
+package me.dielsonsales.app.openpomodoro;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+}

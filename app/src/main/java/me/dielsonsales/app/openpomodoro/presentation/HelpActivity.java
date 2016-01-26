912c16487af6ac52bb2db4e571a9e7d76f50a462
Dielson Sales <dielsonscarvalho@gmail.com>
2016-01-26 08:57:19 -0300

    HelpActivit inherits from BaseActivity

--- app/src/main/java/me/dielsonsales/app/openpomodoro/presentation/HelpActivity.java
+++ app/src/main/java/me/dielsonsales/app/openpomodoro/presentation/HelpActivity.java
@@ -1,31 +1,17 @@
 package me.dielsonsales.app.openpomodoro.presentation;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import me.dielsonsales.app.openpomodoro.R;
 
-public class HelpActivity extends AppCompatActivity {
+public class HelpActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setToolbar(R.id.toolbar, R.string.title_activity_help);
+        setDisplayHomeAsUpEnabled(true);
     }
 
 }
